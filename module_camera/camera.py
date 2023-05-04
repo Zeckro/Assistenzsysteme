16ceb0eb9691ebb421b9df560ca98a889ae29039
Jona-Bro <jona.brockhaus@fh-bielefeld.de>
2023-05-04 13:43:42 +0200

    training dif model sizes;relative coordinates

--- module_camera/camera.py
+++ module_camera/camera.py
@@ -1,6 +1,9 @@
 import paho.mqtt.client as mqtt
 import mediapipe as mp
 import cv2
+import keras
+import tensorflow
+import numpy as np
 
 
 
@@ -25,6 +28,12 @@ class CameraControl:
 
         self.client.connect("localhost", 1883, 60)
 
+        self.model = keras.models.load_model('model0405_relativeTolWrist.h5')
+
+
+        self.actions = ["nextState","rest","screw","solder"] #better solution nec
+
+    """
     def getRealTimeCamera(self,selectData):
         mp_drawing = mp.solutions.drawing_utils
         mp_holistic = mp.solutions.holistic
@@ -69,13 +78,103 @@ class CameraControl:
 
         cap.release()
         cv2.destroyAllWindows()
+        """
+
+    def get_Camera_Activity(self,relativeCoordinates=True,relativeTolwrist=True):
+        mp_drawing = mp.solutions.drawing_utils
+        mp_hand = mp.solutions.hands
+        cap = cv2.VideoCapture(1)
+        # Initiate holistic model
+        data = []
+        with mp_hand.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+            
+            while cap.isOpened():
+                ret, frame = cap.read()
+            
+                
+                # Recolor Feed
+                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                # Make Detections
+                results = holistic.process(image)
+                # print(results.face_landmarks)
+                
+                # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
+                
+                # Recolor image back to BGR for rendering
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                
+
+                if results.multi_hand_landmarks and len(results.multi_hand_landmarks) == 2:
+                    datasetLeftHand = [i for i in results.multi_hand_landmarks[0].landmark]
+                    datasetRightHand = [i for i in results.multi_hand_landmarks[1].landmark]
+                    mp_drawing.draw_landmarks(image, results.multi_hand_landmarks[0], mp_hand.HAND_CONNECTIONS)
+                    mp_drawing.draw_landmarks(image, results.multi_hand_landmarks[1], mp_hand.HAND_CONNECTIONS)
+
+                    dataset = [datasetLeftHand + datasetRightHand]
+                    #print(dataset)
+                    datasettmp = []
+                    if relativeCoordinates:
+                        if relativeTolwrist:
+                            for i in dataset[0]:
+                                datasettmp.append(i.x - dataset[0][0].x)
+                                datasettmp.append(i.y - dataset[0][0].y)
+                                datasettmp.append(i.z)
+                            #tmpx = datasettmp[0]
+                            #tmpy = datasettmp[1]
+                            #datasettmp = [datasettmp[i] - tmpx for i in range(0,len(datasettmp),3)]
+                            #datasettmp = [datasettmp[i+1] - tmpy for i in range(0,len(datasettmp),3)]
+                        else:
+                            tmpCnt = 0
+                            for i in dataset[0]:
+                                if tmpCnt > 0 and tmpCnt < 21:
+                                    datasettmp.append(i.x-datasettmp[0])
+                                    datasettmp.append(i.y-datasettmp[1])
+                                    datasettmp.append(i.z)
+                                elif tmpCnt > 21:
+                                    datasettmp.append(i.x-datasettmp[63])
+                                    datasettmp.append(i.y-datasettmp[64])
+                                    datasettmp.append(i.z)
+                                else:
+                                    datasettmp.append(i.x)
+                                    datasettmp.append(i.y)
+                                    datasettmp.append(i.z)
+                                tmpCnt+=1
+                    else:
+                        for i in dataset[0]:
+
+                            #print(i)
+                            datasettmp.append(i.x)
+                            datasettmp.append(i.y)
+                            datasettmp.append(i.z)
+                    if len(data) == 30:
+                        data.pop(0)
+                    data.append(datasettmp)
+
+                    if len(data) == 30:
+                        prediction = self.model.predict(np.expand_dims(data,axis=0))[0]
+                        print(self.actions[np.argmax(prediction)])
+                    
+                    
+                #os.system('cls')
+                
+            
+                cv2.imshow('camera feed', image)
+                
+
+                if cv2.waitKey(10) & 0xFF == ord('q'):
+                    break
+
+        cap.release()
+        cv2.destroyAllWindows()
+        return data
 
 
 
 
 
 camera1 = CameraControl()
-camera1.getRealTimeCamera(True)
+#camera1.getRealTimeCamera(True)
+camera1.get_Camera_Activity()
 
 
 #camera1.client.loop_forever()
