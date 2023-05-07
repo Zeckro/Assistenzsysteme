e775e50e6616cc1bb04a55e5e925d92ecf394d02
Finn <finn.koehler@fh-bielefeld.de>
2023-05-07 16:14:18 +0200

    add text processing

--- module_speech/SpeechRecognition.py
+++ module_speech/SpeechRecognition.py
@@ -2,8 +2,11 @@ import speech_recognition as sr
 import pyaudio
 import threading
 import paho.mqtt.client as mqtt
+from transformers import pipeline
+import time
 
-WAKE_WORD = "hello"
+WAKE_WORD = "roxy"
+CONFIDENCE = 0.70
 
 class SpeechRecognition:
     #MQTT methods
@@ -21,16 +24,46 @@ class SpeechRecognition:
             print(msg.topic+" "+str(msg.payload))
 
     def __init__(self):
+        print("Initializing...")
+        start = time.time()
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
         self.client.connect("localhost", 1883, 60)
-        print("MQTT-Client initialised")
+        #TODO get task from server
+        self.task = 0
+        self.processor = pipeline(model="facebook/bart-large-mnli")
+        end = time.time()
+        print("MQTT-Client and Processing-Model initialised")
+        print("Init took " + str(end-start) + " seconds")
         self.listen()
 
     # callback function
-    def wake_word_callback(self):
-        print("Wake word detected!")
+    def wake_word_callback(self, text):
+        try:
+            print("Wake word detected! Processing text: "+text)
+            response = self.processor(
+                text,
+                candidate_labels=["forward", "backward", "other"],
+            )
+            print(response)
+            label = response["labels"][0]
+            score = response["scores"][0]
+            if(score > CONFIDENCE and not label =="other"):
+                self.publishTask(True if label == "forward" else False)
+            else:
+                #other
+                pass
+        except:
+            print("Error processing text")
+
+    def publishTask(self, increment):
+        if increment:
+            payload =  str(self.task+1)
+        else:
+            payload = str(self.task-1)
+        print("publishing with payload: " + payload)
+        self.client.publish("speech_module/task", increment)
 
     def play_audio(self,audio_data):
         try:
@@ -61,19 +94,23 @@ class SpeechRecognition:
 
             while True:
                 # Listen for audio input
-                audio = r.listen(source)
+                #audio = r.listen(source)
+                #TODO improve listening
+                audio = r.listen(source,5,5)
                 # DEBUG -- play audio in separate thread
-                t = threading.Thread(target=SpeechRecognition.play_audio, args=(self,audio,))
-                t.start()
+                #t = threading.Thread(target=SpeechRecognition.play_audio, args=(self,audio,))
+                #t.start()
 
                 try:
                     #  google speech recognitoin TODO funktionieren die andere besser?
-                    text = r.recognize_google(audio)
+                    text = r.recognize_google(audio).lower()
                     print(text)
                     # Check if the wake word is in the transcribed text
-                    if WAKE_WORD in text.lower():
-                        self.wake_word_callback()
-
+                    if WAKE_WORD in text:
+                        #only process text after wake word
+                        text = text.partition(WAKE_WORD)[2]
+                        t = threading.Thread(target=SpeechRecognition.wake_word_callback, args=(self,text,))
+                        t.start()
                 except sr.UnknownValueError:
                     # audio not recognised
                     print("Could not understand audio")
