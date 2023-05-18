9f3e0c670aa0ea5c6ee213d685abfa7843b35533
Jona-Bro <jona.brockhaus@fh-bielefeld.de>
2023-05-18 11:26:17 +0200

    time analysis, mqtt sending of image and "task done"

--- module_camera/camera.py
+++ module_camera/camera.py
@@ -7,23 +7,29 @@ import numpy as np
 import os
 import pickle
 import time
-
-
+import threading
+import json
+#from main import Task, AssemblyList
+#import main.py
 
 class CameraControl:
     #MQTT methods
     def on_connect(self,client, userdata, flags, rc):
 
         print("Connected with result code " + str(rc))
-        client.subscribe("test/topic")
-        client.subscribe("test2/topic")
+        client.subscribe("master/current_task")
+       
 
 
     def on_message(self,client, userdata, msg):
-        if msg.topic == "test/topic":
+        print("Message received: ")
+        if msg.topic == "master/current_task":
+            print(msg.payload)
+            print(type(msg.payload))
+            jsondict = json.loads(msg.payload)
+            self.currentTask = jsondict["name"]
+        else:
             print(msg.topic+" "+str(msg.payload))
-        elif msg.topic == "test2/topic":
-            print("Test2")
 
     def __init__(self):
         self.client = mqtt.Client()
@@ -32,68 +38,29 @@ class CameraControl:
 
         self.client.connect("localhost", 1883, 60)
         dir_path = os.path.dirname(os.path.realpath(__file__))  
-        self.path = os.path.join(dir_path, 'model0905_relativeTolWrist_smallDropout.h5')
+        self.path = os.path.join(dir_path, 'model1105_relativeTolWrist_bigDensedropout.h5')
         self.model = keras.models.load_model(self.path)
         self.sequence_length = self.model.layers[0].input_shape[1]
+        self.TaskPerformed = False
+        self.currentTask = ""
 
         with open('module_camera\\mappingActivity.pkl', 'rb') as f:
             mappingActivity = pickle.load(f)
             self.actions = [mappingActivity[i] for i in sorted(mappingActivity.keys(), reverse=False)]
             print(self.actions)
 
-    """
-    def getRealTimeCamera(self,selectData):
-        mp_drawing = mp.solutions.drawing_utils
-        mp_holistic = mp.solutions.holistic
-        cap = cv2.VideoCapture(0)
-        # Initiate holistic model
-        with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-            
-            while cap.isOpened() and selectData:
-                ret, frame = cap.read()
-                
-                # Recolor Feed
-                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                # Make Detections
-                results = holistic.process(image)
-                # print(results.face_landmarks)
-                
-                # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
-                
-                # Recolor image back to BGR for rendering
-                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-                
-                # Draw face landmarks
-                #mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
-                
-                # Right hand
-                mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        activityRecognitionTask = threading.Thread(target=self.get_Camera_Activity)
+        activityRecognitionTask.daemon = True
+        activityRecognitionTask.start()
 
-                # Left Hand
-                mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-
-                # Pose Detections
-                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
-
-                #print(type(results.left_hand_landmarks))
-                print(results.left_hand_landmarks)
-
-                                
-                cv2.imshow('Raw Webcam Feed', image)
-
-                if cv2.waitKey(10) & 0xFF == ord('q'):
-                    break
-
-        cap.release()
-        cv2.destroyAllWindows()
-        """
 
     def get_Camera_Activity(self,relativeCoordinates=True,relativeTolwrist=True):
         mp_drawing = mp.solutions.drawing_utils
         mp_hand = mp.solutions.hands
-        cap = cv2.VideoCapture(1)
+        cap = cv2.VideoCapture(0)
         # Initiate holistic model
         data = []
+        
         with mp_hand.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
             
             while cap.isOpened():
@@ -103,8 +70,11 @@ class CameraControl:
                 
                 # Recolor Feed
                 image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                timeImageAcquisition = time.time() - timeLoopBegin
                 # Make Detections
                 results = holistic.process(image)
+
+                timeImagePoseEst = time.time() - timeLoopBegin
                 # print(results.face_landmarks)
                 
                 # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
@@ -159,35 +129,70 @@ class CameraControl:
                         data.pop(0)
                     data.append(datasettmp)
 
-                    if len(data) == self.sequence_length:
+                    timeDataPreProcessing = time.time() - timeLoopBegin
+
+                    if len(data) == self.sequence_length and self.currentTask != "":
                         prediction = self.model.predict(np.expand_dims(data,axis=0))[0]
-                        print(self.actions[np.argmax(prediction)])
+                        self.recognizedTask = self.actions[np.argmax(prediction)]
+
+                        timeTaskPredicition = time.time() - timeLoopBegin
+
+                        print( "Recognition: " + self.recognizedTask + " ---- Task of Assembly List: " + self.currentTask)
+                        if self.recognizedTask == self.currentTask:
+                            self.TaskPerformed = True
+                        elif self.TaskPerformed and self.recognizedTask != self.currentTask:
+                            self.TaskPerformed = False
+                            self.client.publish("master/next_task","NOT IMPLEMENTED YET")
+                            print("MQTT Publish was performed!")
+
+                        _, img_encoded = cv2.imencode('.jpg', frame)
+                        byte_array = img_encoded.tobytes()
+                        self.client.publish("image_topic", byte_array)
+
+                        timeImageSending = time.time() - timeLoopBegin
+                        print("Timing")
+                        print("Image Ac.: "+str(timeImageAcquisition) 
+                              + " Pose Est.: "+str(timeImagePoseEst-timeImageAcquisition)
+                              + " Preproc: "+str(timeDataPreProcessing-timeImagePoseEst)
+                              + " Task Pred.: "+str(timeTaskPredicition-timeDataPreProcessing)
+                              + " Sending: "+str(timeImageSending-timeTaskPredicition))
+
                     
                     
                 #os.system('cls')
                 
             
-                cv2.imshow('camera feed', image)
-                timeLoopDif = time.time() - timeLoopBegin
-                print("FPS" + str(int(1/timeLoopDif)))
+                #cv2.imshow('camera feed', image)
+                #timeLoopDif = time.time() - timeLoopBegin
+                #print("FPS" + str(int(1/timeLoopDif)))
 
+                    
                 if cv2.waitKey(10) & 0xFF == ord('q'):
                     break
 
         cap.release()
         cv2.destroyAllWindows()
         return data
+    
+        """
+        TO DO:
+        run programm on another pc -> checking frame rate
+        possible solutions if fps still to low:
+        -minimize neural network size -> maybe 10-20ms gain
+        -run predicition and pose estimation in two seperate threads -> at least mqtt rate is high
+        """
 
 
 
 
-
-camera1 = CameraControl()
-#camera1.getRealTimeCamera(True)
-camera1.get_Camera_Activity()
-
-
-#camera1.client.loop_forever()
-
+if __name__ == '__main__':
+    camera = CameraControl()
+    #camera.get_Camera_Activity()
+    #activityRecognitionTask = threading.Thread(target=CameraControl.get_Camera_Activity)
+    #activityRecognitionTask.daemon = True
+    #activityRecognitionTask.start()
+    camera.client.loop_forever()
+    #activityRecognitionTask.stop()
+    
 
 
